e831f856956d88a3bd5b93032e74c88838c071a8
jamesfeatherston <jamesfeatherston@utexas.edu>
2025-12-17 14:26:35 -0600

    Next.js installation

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Application Tracker",
+  description: "Track your job applications",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
